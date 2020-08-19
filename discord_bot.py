572f5fdc2e0c5841a1f128533bc8a1b3675d3f47
Ibrahim Cetin <cetinibrahim@yahoo.com>
2020-08-19 22:46:04 +0300

    emoji.id replaced to emoji.name

--- discord_bot.py
+++ discord_bot.py
@@ -11,25 +11,25 @@ class MyBot(commands.Bot):
 
     async def on_raw_reaction_add(self, payload):
         if payload.message_id == "message_id":
-            emoji_id = str(payload.emoji.id)
+            emoji_name = str(payload.emoji.name)
 
-            if emoji_id in self.emojis_json_data.keys():
+            if emoji_name in self.emojis_json_data.keys():
                 role = discord.utils.get(
-                    payload.member.guild.roles, name=self.emojis_json_data[emoji_id]
+                    payload.member.guild.roles, name=self.emojis_json_data[emoji_name]
                 )
 
                 await payload.member.add_roles(role)
 
     async def on_raw_reaction_remove(self, payload):
         if payload.message_id == "message_id":
-            emoji_id = str(payload.emoji.id)
+            emoji_name = str(payload.emoji.name)
 
-            if emoji_id in self.emojis_json_data.keys():
+            if emoji_name in self.emojis_json_data.keys():
                 guild = self.get_guild(payload.guild_id)
                 member = guild.get_member(payload.user_id)
                 roles = guild.roles
 
-                role = discord.utils.get(roles, name=self.emojis_json_data[emoji_id])
+                role = discord.utils.get(roles, name=self.emojis_json_data[emoji_name])
 
                 await member.remove_roles(role)
 
